ac31b1600fe2bf5cb023465141145ae232b1f7e4
Faisal Ali <fzl.ali33@gmail.com>
2022-03-26 18:11:56 +0530

    Setting up Files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# key_extractor
+# KEY Extractor
 A Python module for extracting keys from a YAML file.
---
+++ key_extractor.py
@@ -0,0 +1,7 @@
+# Author: Faisal Ali
+# Creation Date: Mar, 26 2022
+# Version: 0.1
+# Revision Date: NA
+
+# Import libraries
+import ruamel.yaml
