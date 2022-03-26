f1eac3b6c2cf472acfd56d6f8bdfe92744554abc
Faisal Ali <fzl.ali33@gmail.com>
2022-03-26 23:17:21 +0530

    Added to_text function
    Keys can be saved to a text file.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # KEY Extractor
 A Python module for extracting keys from a YAML file.
+If a YAML contains a nested hierarchy then the complete path is listed from parent to child key.
--- key_extractor.py
+++ key_extractor.py
@@ -1,17 +1,16 @@
 # Author: Faisal Ali
 # Creation Date: Mar, 26 2022
-# Version: 0.1.2
-# Revision Date: NA
+# Version: 0.1.3
+# Revision Date: Mar, 26 2022
 
 # Import libraries
-from tkinter.tix import TEXT
 import ruamel.yaml
 
 # Creating an empty list for storing keys
 yaml_keys = list()
 
 
-def reader(filename: str) -> dict:
+def read_yaml(filename: str) -> dict:
     """
     Takes the path and filename for the YAML, reads the same and returns a python dictionary.
 
@@ -32,7 +31,7 @@ def reader(filename: str) -> dict:
         quit()
 
 
-def key_extraction_yaml_obj(yaml_dict: dict, prefix="") -> list:
+def get_keys(yaml_dict: dict, sep=">", prefix="") -> list:
     """
     Iterate over all the key value pairs in a YAML dictionary and returns the keys.
 
@@ -48,19 +47,19 @@ def key_extraction_yaml_obj(yaml_dict: dict, prefix="") -> list:
                 if res == True:
                     tmp1 = p2.strip()
                     tmp2 = tmp1.split(" ")
-                    yaml_keys.append(">".join(tmp2))
+                    yaml_keys.append(sep.join(tmp2))
                 else:
-                    key_extraction_yaml_obj(v2, p2)
+                    get_keys(v2, sep, p2)
 
         elif isinstance(yaml_dict, list):
             for i, v2 in enumerate(yaml_dict):
                 p2 = f"{prefix} {i}"
-                key_extraction_yaml_obj(v2, p2)
+                get_keys(v2, sep, p2)
 
         else:
             tmp1 = prefix.strip()
             tmp2 = tmp1.split(" ")
-            yaml_keys.append(">".join(tmp2))
+            yaml_keys.append(sep.join(tmp2))
 
         return yaml_keys
 
@@ -69,15 +68,15 @@ def key_extraction_yaml_obj(yaml_dict: dict, prefix="") -> list:
         quit()
 
 
-def key_extraction_yaml_file(filename: str) -> list:
+def to_text(data: list, filename: str) -> object:
     """
-    Iterate over all the key value pairs in a YAML file and returns the keys.
+    Saves the data to a text file.
 
-    filename: path and filename for YAML.
+    data: Data in the form of list (YAML Keys)
+    filename: path and filename of the TXT file
 
-    return: list of keys from the YAML.
+    return: TXT File
     """
-
-    yaml_object = reader(filename)
-    keys = key_extraction_yaml_obj(yaml_dict=yaml_object)
-    return keys
+    with open(filename, mode="w+") as txt_file:
+        for elm in data:
+            txt_file.write(f"{elm}\n")
